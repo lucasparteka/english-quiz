285a0d335e863f523aff702f80c1ad6e734f5b51
lucasparteka <41965553+lucasparteka@users.noreply.github.com>
2024-10-31 16:16:00 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   distDir: 'dist',
-  output: 'standalone'
+  output: 'standalone',
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
