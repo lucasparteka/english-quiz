0225eda36ac2da7fada987e17d2ce61b690e623a
Lucas Parteka Leal <lucasparteka@gmail.com>
2024-10-31 17:09:12 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  distDir: 'dist',
-  output: 'standalone',
+  output: "standalone",
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
